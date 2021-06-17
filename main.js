da72bec19eb1fcf92eeec304dcf705666e30f54f
Mohitha2008 <84503912+Mohitha2008@users.noreply.github.com>
2021-06-17 17:19:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    clown_nose=loadImage('https://i.postimg.cc/1tLJ5600/take-this-and-deleteit.png');
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotResult);
+}
+function draw(){
+    image(video,0,0,300,300);
+    image(clown_nose, noseX, noseY, 30,30);
+}
+function take_snapshot(){
+    save('joker_filter.png');
+}
+
+function modelLoaded(){
+    console.log("Model loaded!");
+}
+function gotResult(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y-2;
+        console.log(" nose y ~  "+noseY);
+        console.log(" nose x ~  "+noseX);
+    }
+}
